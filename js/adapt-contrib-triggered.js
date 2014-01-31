507fc71b7b7064a743a62c15cd3794baad297c30
Daryl Hedley <darylhedley@hotmail.com>
2014-01-31 15:38:21 +0000

    Initial commit of plugin code

--- js/adapt-contrib-triggered.js
+++ js/adapt-contrib-triggered.js
@@ -7,12 +7,54 @@ define(function(require) {
 
     var Adapt = require('coreJS/adapt');
 
-    function setupTriggeredView (view) {
+    function setupTriggeredViews(componentView) {
 
-        var TriggeredView = Backbone.View.extend({
+        var TriggeredShowView = Backbone.View.extend({
+
+            tagName: 'a',
+
+            className: 'triggered-button-show',
+
+            initialize: function() {
+                this.listenTo(Adapt, 'remove', this.remove);
+                this.$el.attr('data-triggered-id', this.model.get('_id')).attr('href', '#');
+                this.triggeredView = componentView;
+                this.render();
+            },
+
+            events: {
+                'click': 'show'
+            },
+
+            render: function() {
+                var data = this.model.toJSON();
+                var template = Handlebars.templates['triggered-show-button'];
+                this.$el.html(template(data)).css({
+                    left:componentView.model.get('triggered').left + "%", 
+                    top:componentView.model.get('triggered').top + "%"
+                }).appendTo(componentView.options.$parent.addClass('triggered-container'));
+                return this;
+            },
+
+            show: function(event) {
+                event.preventDefault();
+                var $currentTarget = $(event.currentTarget);
+                var currentTriggeredId = $currentTarget.attr('data-triggered-id');
+
+                $currentTarget.addClass('triggered-hidden');
+                
+                this.triggeredView.model.set('_isVisible', true);
+                $('.' + currentTriggeredId).removeClass('triggered-hidden');
+                this.triggeredView.$el.data('inview', false);
+                $(window).scroll();
+            }
+
+        });
+
+        var TriggeredHideView = Backbone.View.extend({
 
             el: function() {
-                return view.options.$parent;
+                return componentView.el;
             },
 
             initialize: function() {
@@ -21,42 +63,40 @@ define(function(require) {
             },
 
             events: {
-                'click .triggered-button-show': 'show',
                 'click .triggered-button-hide': 'hide'
             },
 
             render: function() {
                 var data = this.model.toJSON();
-                var hideTemplate = Handlebars.templates['triggered-hide-button'];
-                var showTemplate = Handlebars.templates['triggered-show-button'];
-
-                view.$el.addClass('triggered-hidden triggered-component').append(hideTemplate(data));
-                $(showTemplate(data)).css({
-                    left:view.model.get('triggered').left, 
-                    top:view.model.get('triggered').top
-                }).appendTo(this.$el.addClass('triggered-container'));
+                var template = Handlebars.templates['triggered-hide-button'];
+                this.$el.addClass('triggered-component triggered-hidden').append(template(data));
                 return this;
-            },
-
-            show: function(event) {
-                event.preventDefault();
-                $('.' + $(event.currentTarget).attr('data-id')).removeClass('triggered-hidden');
             },
 
             hide: function(event) {
                 event.preventDefault();
-                view.$el.addClass('triggered-hidden');
+                var currentTriggeredId = $(event.currentTarget).attr('data-triggered-id');
+                this.model.set('_isVisible', false);
+                this.$el.addClass('triggered-hidden');
+                $('.triggered-button-show[data-triggered-id="'+currentTriggeredId+'"]').removeClass('triggered-hidden');
             }
 
         });
 
-        new TriggeredView({model: view.model});
+        new TriggeredShowView({model: componentView.model});
+        new TriggeredHideView({model: componentView.model});
 
     }
 
-    Adapt.on('componentView:postRender', function(view) {
+    Adapt.on('componentView:preRender', function(view) {
         if (view.model.get('triggered')) {
-            setupTriggeredView(view);
+            view.model.set('_isVisible', false);
+        }
+    });
+
+    Adapt.on('componentView:postRender', function(componentView) {
+        if (componentView.model.get('triggered')) {
+            setupTriggeredViews(componentView);
         }
     });
 
