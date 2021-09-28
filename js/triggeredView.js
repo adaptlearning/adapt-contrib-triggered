ac47fd317e627b34b81f679ccd51f2d180a0ff42
Eleanor Heath <eleanor.heath@learningpoool.com>
2021-09-28 15:27:30 +0100

    2808 - var to const

--- js/triggeredView.js
+++ js/triggeredView.js
@@ -27,9 +27,9 @@ define([
     }
 
     render() {
-      var data = this.triggeredView.model.toJSON();
-      var template = Handlebars.templates[this.template];
-      var $container = $('.component__container', $('.' + this.triggeredView.model.get('_parentId')));
+      const data = this.triggeredView.model.toJSON();
+      const template = Handlebars.templates[this.template];
+      const $container = $('.component__container', $('.' + this.triggeredView.model.get('_parentId')));
       $container
         .addClass('triggered__hidden')
         .append(this.$el.html(template(data)));
@@ -37,11 +37,11 @@ define([
 
     show(event) {
       event && event.preventDefault();
-      var $currentTarget = $(event.currentTarget);
-      var currentTriggeredId = this.triggeredView.model.get('_id');
+      const $currentTarget = $(event.currentTarget);
+      const currentTriggeredId = this.triggeredView.model.get('_id');
       this.triggeredView.model.set('_isVisible', true, { pluginName: '_triggered' });
       $currentTarget.addClass('u-display-none');
-      var $container = $('.component__container', $('.' + this.triggeredView.model.get('_parentId')));
+      const $container = $('.component__container', $('.' + this.triggeredView.model.get('_parentId')));
       $container.removeClass('triggered__hidden');
       $(`.triggered__button-hide[data-triggered-id='${currentTriggeredId}']`)
         .removeClass('u-display-none');
@@ -52,11 +52,11 @@ define([
 
     hide(event) {
       event && event.preventDefault();
-      var $currentTarget = $(event.currentTarget);
+      const $currentTarget = $(event.currentTarget);
       const currentTriggeredId = this.triggeredView.model.get('_id');
       this.triggeredView.model.set('_isVisible', false, { pluginName: '_triggered' });
       $currentTarget.addClass('u-display-none');
-      var $container = $('.component__container', $('.' + this.triggeredView.model.get('_parentId')));
+      const $container = $('.component__container', $('.' + this.triggeredView.model.get('_parentId')));
       $container.addClass('triggered__hidden');
       $(`.triggered__button-show[data-triggered-id='${currentTriggeredId}']`)
         .removeClass('u-display-none');
