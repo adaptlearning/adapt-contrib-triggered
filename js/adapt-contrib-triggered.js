20c611f37729f2b1478d814929768462ef290672
Eleanor Heath <eleanor.heath@learningpoool.com>
2021-09-28 15:30:57 +0100

    2808 - added imports/exports

--- js/adapt-contrib-triggered.js
+++ js/adapt-contrib-triggered.js
@@ -1,30 +1,26 @@
-define([
-  'coreJS/adapt',
-  './triggeredView'
-], function(Adapt, TriggeredView) {
+import Adapt from 'coreJS/adapt';
+import TriggeredView from './triggeredView';
 
-  class Triggered extends Backbone.Controller {
+class Triggered extends Backbone.Controller {
 
-    initialize() {
-      this.listenTo(Adapt, 'app:dataReady', this.onDataReady);
-    }
-
-    onDataReady() {
-      if (!Adapt.course.get('_triggered') || !Adapt.course.get('_triggered')._isEnabled) {
-        return;
-      }
-
-      this.listenTo(Adapt, {
-        remove: this.remove,
-        'componentView:postRender': this.onComponentViewPostRender
-      });
-    }
-
-    onComponentViewPostRender(componentView) {
-      new TriggeredView({ componentView });
-    }
+  initialize() {
+    this.listenTo(Adapt, 'app:dataReady', this.onDataReady);
   }
 
-  return new Triggered();
+  onDataReady() {
+    if (!Adapt.course.get('_triggered') || !Adapt.course.get('_triggered')._isEnabled) {
+      return;
+    }
 
-});
+    this.listenTo(Adapt, {
+      remove: this.remove,
+      'componentView:postRender': this.onComponentViewPostRender
+    });
+  }
+
+  onComponentViewPostRender(componentView) {
+    new TriggeredView({ componentView });
+  }
+}
+
+export default new Triggered();
